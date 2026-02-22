818866e402a4fcddfe213c300103ee63b1d7cce3
Idar Lafish <idarlafish@pm.me>
2026-02-22 23:39:25 +0200

    feat(infra): add module server resource

---
+++ infra/modules/hcloud-server/main.tf
@@ -0,0 +1,25 @@
+resource "hcloud_server" "this" {
+  name        = var.name
+  server_type = var.server_type
+  image       = "ubuntu-24.04"
+  backups     = true
+  ssh_keys    = [var.ssh_key_id]
+  location    = var.location
+
+  network {
+    network_id = hcloud_network.this.id
+    ip         = var.private_ip
+  }
+
+  firewall_ids = concat(
+    [hcloud_firewall.base.id],
+    var.extra_firewall_ids
+  )
+
+  user_data = var.cloud_init
+
+  depends_on = [
+    hcloud_network_subnet.this,
+    hcloud_firewall.base,
+  ]
+}
