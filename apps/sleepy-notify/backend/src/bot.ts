a2a6fd8365d167bfc4729a22b46bc7a3e1fcf10c
Idar Lafish <idarlafish@pm.me>
2026-04-26 18:44:26 +0300

    chore: remove apps/sleepy-notify (replaced by telegram-notify)

--- apps/sleepy-notify/backend/src/bot.ts
+++
@@ -1,6 +0,0 @@
-import { Bot } from "grammy";
-import { config } from "./config";
-
-export const bot = new Bot(config.BOT_TOKEN);
-
-export type BotContext = typeof bot extends Bot<infer C> ? C : never;
