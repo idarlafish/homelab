19aa5ddcc3866091946155893f470bb579c8fb27
Idar Lafish <idarlafish@pm.me>
2026-04-26 18:44:26 +0300

    chore: remove apps/sleepy-notify (replaced by telegram-notify)

--- apps/sleepy-notify/backend/src/commands/done.ts
+++
@@ -1,7 +0,0 @@
-import { bot } from "../bot";
-
-bot.callbackQuery("done", async (ctx) => {
-	await ctx.deleteMessage();
-	await ctx.answerCallbackQuery({ text: "✓ Done!" });
-	console.log(`🗑️ User completed notification`);
-});
