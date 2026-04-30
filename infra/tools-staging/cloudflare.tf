24c20eb0144ddd307bfdaa346c71e6a2829ac118
Idar Lafish <idarlafish@pm.me>
2026-04-30 21:06:08 +0300

    Revert "fix(tools-staging): create staging.la.fish zone for proper Universal SSL"
    This reverts commit eaf453b16d9d778ecd66290876e02cdea2ad2348.

--- infra/tools-staging/cloudflare.tf
+++ infra/tools-staging/cloudflare.tf
@@ -4,19 +4,6 @@ data "cloudflare_zone" "main" {
   }
 }
 
-# `staging.la.fish` is its own Cloudflare zone so Universal SSL covers
-# `<host>.staging.la.fish` (free Universal SSL is one-level only;
-# `*.staging.la.fish` is two levels under `la.fish` so it doesn't get
-# auto-issued there). Cloudflare auto-delegates child zones inside the
-# same account.
-resource "cloudflare_zone" "staging" {
-  account = {
-    id = data.sops_file.secrets.data["cloudflare_account_id"]
-  }
-  name = "staging.${data.sops_file.secrets.data["cloudflare_zone_name"]}"
-  type = "full"
-}
-
 resource "random_id" "tunnel_secret" {
   byte_length = 32
 }
@@ -35,23 +22,23 @@ resource "cloudflare_zero_trust_tunnel_cloudflared_config" "this" {
   config = {
     ingress = [
       {
-        hostname = "auth.staging.${data.sops_file.secrets.data["cloudflare_zone_name"]}"
+        hostname = "auth-staging.${data.sops_file.secrets.data["cloudflare_zone_name"]}"
         service  = "http://pocket-id.identity.svc.cluster.local:1411"
       },
       {
-        hostname = "booklore.staging.${data.sops_file.secrets.data["cloudflare_zone_name"]}"
+        hostname = "booklore-staging.${data.sops_file.secrets.data["cloudflare_zone_name"]}"
         service  = "http://booklore.booklore.svc.cluster.local:6060"
       },
       {
-        hostname = "wg-admin.staging.${data.sops_file.secrets.data["cloudflare_zone_name"]}"
+        hostname = "wg-admin-staging.${data.sops_file.secrets.data["cloudflare_zone_name"]}"
         service  = "http://wg-easy-http.vpn.svc.cluster.local:51821"
       },
       {
-        hostname = "grafana.staging.${data.sops_file.secrets.data["cloudflare_zone_name"]}"
+        hostname = "grafana-staging.${data.sops_file.secrets.data["cloudflare_zone_name"]}"
         service  = "http://kube-prometheus-stack-grafana.monitoring.svc.cluster.local:80"
       },
       {
-        hostname = "prometheus.staging.${data.sops_file.secrets.data["cloudflare_zone_name"]}"
+        hostname = "prometheus-staging.${data.sops_file.secrets.data["cloudflare_zone_name"]}"
         service  = "http://kube-prometheus-stack-prometheus.monitoring.svc.cluster.local:9090"
       },
       {
@@ -63,19 +50,19 @@ resource "cloudflare_zero_trust_tunnel_cloudflared_config" "this" {
 
 locals {
   tunnel_hostnames = [
-    "auth",
-    "booklore",
-    "wg-admin",
-    "grafana",
-    "prometheus",
+    "auth-staging",
+    "booklore-staging",
+    "wg-admin-staging",
+    "grafana-staging",
+    "prometheus-staging",
   ]
 }
 
 resource "cloudflare_dns_record" "tunnel" {
   for_each = toset(local.tunnel_hostnames)
 
-  zone_id = cloudflare_zone.staging.id
-  name    = "${each.value}.${cloudflare_zone.staging.name}"
+  zone_id = data.cloudflare_zone.main.zone_id
+  name    = "${each.value}.${data.sops_file.secrets.data["cloudflare_zone_name"]}"
   type    = "CNAME"
   content = "${cloudflare_zero_trust_tunnel_cloudflared.this.id}.cfargotunnel.com"
   ttl     = 1
